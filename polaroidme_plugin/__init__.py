606a902fa8d6a57b0cb50c9b0acd38c11b13e379
s3h10r <sven.hessenmueller@gmail.com>
2019-07-22 12:30:52 +0200

    finishes polaroidme plugin update

--- polaroidme_plugin/__init__.py
+++ polaroidme_plugin/__init__.py
@@ -1,11 +1,17 @@
-from ..randomart import generate_image as _generate_image
+#!/usr/bin/env python
+#coding=utf-8
+
+# === polaroidme plugin-interface ===
 # --- all polaroidme-plugins (generators, filters) must implement this
+import logging
+import string
+
 name = "psychedelic"
 description = "Random (Psychedelic) Art"
 kwargs = {'pixels_per_unit' : 150, 'seed' : None}
 args=None
 author = "Jeremy Kun http://jeremykun.com/2012/01/01/random-psychedelic-art/"
-version = '0.2.0'
+version = '0.4.0'
 
 def run(**kwargs):
     """
@@ -16,4 +22,34 @@ def run(**kwargs):
         return _generate_image()
     else:
         return _generate_image(**kwargs)
+
 # --- END all polaroidme-plugins (generators, filters) must implement this
+
+def get_plugin_doc(format='text'):
+    """
+    """
+    if format not in ('txt', 'text', 'plaintext'):
+        raise Exception("Sorry. format %s not available. Valid options are ['text']" % format)
+    tpl_doc = string.Template("""
+    filters.$name - $description
+    kwargs  : $kwargs
+    args    : $args
+    author  : $author
+    version : __version__
+    """)
+    return tpl_doc.substitute({
+        'name' : name,
+        'description' : description,
+        'kwargs' : kwargs,
+        'args'    : args,
+        'author'  : author,
+        'version' : __version__,
+        })
+
+if __name__ == '__main__':
+    print(get_plugin_doc())
+
+# === END polaroidme plugin-interface
+
+# --- .. here comes the plugin-specific part to get some work done...
+from ..randomart import generate_image as _generate_image
